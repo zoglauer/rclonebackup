3e30584a424bdbb93c51aa22d7f29b4a06b89ac0
Andreas Zoglauer <andreas@megalibtoolkit.com>
2024-07-03 12:44:32 -0700

    CHG: Handle copy errors

--- backup_rclone_wasabi.sh
+++ backup_rclone_wasabi.sh
@@ -229,7 +229,10 @@ echo " " 2>&1 | tee -a ${LOG}
 
 # 2022/2/12: Copy links as .rclonelink to avoid dangling links
 OPTIONS="--config ${RCLONECONFIG} -P --stats 1m -l --fast-list --transfers=2 --check-first --backup-dir ${BACKUPDIFFDIR} ${FILTER} ${EXCLUDE} sync ${RAIDDIR} ${BACKUPDIR}"
+# 2024/6/20: Wasabi transfer speed up
 OPTIONS="--config ${RCLONECONFIG} -P --stats 1m -l --fast-list --transfers=32 --checkers=32 --backup-dir ${BACKUPDIFFDIR} ${FILTER} ${EXCLUDE} sync ${RAIDDIR} ${BACKUPDIR}"
+# Limit server side copy size:
+OPTIONS="--config ${RCLONECONFIG} -P --stats 1m -l --fast-list --transfers=32 --checkers=32 --s3-copy-cutoff 2000M --backup-dir ${BACKUPDIFFDIR} ${FILTER} ${EXCLUDE} sync ${RAIDDIR} ${BACKUPDIR}"
 if [[ ${VERBOSE} == "FALSE" ]]; then
   OPTIONS="--stats-one-line ${OPTIONS}"
 fi
